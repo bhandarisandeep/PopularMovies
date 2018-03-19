66d087fdc57780a361de176e7be8682ef19b140b
bhandarisandeep <sandeepbhandari033@gmail.com>
2018-03-19 15:40:34 +0530

    this commit is done when the jason data is retriveing in textField

--- app/src/main/java/com/example/sandeep/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/sandeep/popularmovies/MainActivity.java
@@ -1,18 +1,70 @@
 package com.example.sandeep.popularmovies;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.sandeep.popularmovies.utilities.NetworkUtils;
 
+import java.io.IOException;
+import java.net.URL;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv_showData;
+    ProgressBar spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    tv_showData=(TextView)findViewById(R.id.tv_showData);
+    spinner=(ProgressBar) findViewById(R.id.spinner);
+
+
+    }
+    public class MovieTask extends AsyncTask<URL ,Void, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            spinner.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL popularUrl = urls[0];
+            String movieSearchResults = callHttp(popularUrl);
+            return movieSearchResults;
+        }
+            /*this is the other way of doing without the method callhttp
+*   String movieSearchResults = null;
+*          try {
+*               movieSearchResults = NetworkUtils.getResponseFromHttpUrl(popularUrl);
+*               //tv_showData.setText(movieSearchResults);
+*          } catch (IOException e) {
+*               e.printStackTrace();
+*
+*           }
+* return movieSearchResults;
+*/
+        @Override
+        protected void onPostExecute(String movieSearchReults) {
+           spinner.setVisibility(View.INVISIBLE);
+            if(movieSearchReults!=null && !movieSearchReults.equals("")){
+                tv_showData.setText(movieSearchReults);
+
+            }
+        }
+
     }
 
 
@@ -31,7 +83,12 @@ public class MainActivity extends AppCompatActivity {
         if(ItemOfMenu==R.id.mostPopular)
         {
             Context context=MainActivity.this;
-            Toast.makeText(this, "You clicked most popular", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, "You clicked most popular", Toast.LENGTH_SHORT).show();
+            URL popularMoviesUrl = NetworkUtils.urlForPopular();
+            tv_showData.setText(popularMoviesUrl.toString()+"\n\n\n");
+            //tv_showData.setText(callHttp(popularMoviesUrl).toString());  // NOTES: By using this we will get a NetworkOnMainthread error which will solve by using AsyncTask
+            new MovieTask().execute(popularMoviesUrl);
+
         }
         if (ItemOfMenu==R.id.topRated)
         {
@@ -41,4 +98,21 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
 
-}}
+}
+/*
+
+this method(callhttp) is used for to get the json data from the url
+*/
+        public String callHttp(URL fullUrl){
+        String SearchResults = null;
+        try {
+        SearchResults = NetworkUtils.getResponseFromHttpUrl(fullUrl);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                    }
+                return SearchResults;
+
+        }
+
+}
